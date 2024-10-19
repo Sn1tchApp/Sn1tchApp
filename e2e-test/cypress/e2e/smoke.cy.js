75457f23a7f29fdac3d9cea370026d1da0d16dd1
Marcelo Lima <marcelo.suporte.redes@gmail.com>
2024-10-19 13:45:52 -0300

    Ajsute de teste

--- e2e-test/cypress/e2e/smoke.cy.js
+++ e2e-test/cypress/e2e/smoke.cy.js
@@ -1,5 +1,21 @@
-describe("template spec", () => {
-  it("passes", () => {
+context("Dado que não fiz login na plataforma", () => {
+  beforeEach(() => {
+    cy.clearCookies();
     cy.visit("https://sn1tch-app-github-contribs-front.vercel.app/login");
+      cy.contains("h1", "Sn1tch Auth");
+      cy.get("button").contains("Login").click();
+      cy.origin("https://github.com", () => {
+        cy.get('input[name="login"]').type(Cypress.env('GITHUB_USERNAME'));
+        cy.get('input[name="password"]').type(Cypress.env('GITHUB_PASSWORD'), { log: false });
+      
+        cy.get('input[type="submit"]').click();
+      })
+      cy.get("button").contains("Sign in").click();
   });
-});
+  describe("Quando acesso a plataforma", () => {
+    it("Devo conseguir fazer login e visualizar a homepage com botão de cadastrar", () => {
+      cy.contains("h1", "Projetos");
+    });
+  });
+})
+
