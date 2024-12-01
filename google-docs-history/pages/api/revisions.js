5827c582947698bf15aaa7ba2acba40f5cf06a6f
Marcelo Lima <marcelo.suporte.redes@gmail.com>
2024-12-01 10:45:39 -0300

    Ajustes para carregar autenticação com o google via variáveis de ambiente

--- google-docs-history/pages/api/revisions.js
+++ google-docs-history/pages/api/revisions.js
@@ -9,16 +9,48 @@ export default async function handler(req, res) {
 
   try {
     console.log("API Drive Activity sendo chamada");
-
+    console.log("Autenticação configurada:", process.env.GOOGLE_PRIVATE_KEY_ID);
+    console.log("Autenticação configurada:", process.env.GOOGLE_PRIVATE_KEY);
+    console.log("Autenticação configurada:", process.env.GOOGLE_CLIENT_EMAIL);
+    console.log("Autenticação configurada:", process.env.GOOGLE_CLIENT_ID);
     // Configurar autenticação para Drive Activity
     const driveAuth = new google.auth.GoogleAuth({
-      keyFile: path.join(process.cwd(), process.env.SERVICE_ACCOUNT_KEY),
+      credentials: {
+        type: "service_account",
+        project_id: "sn1tchapp",
+        private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+        private_key: process.env.GOOGLE_PRIVATE_KEY,
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        auth_uri: "https://accounts.google.com/o/oauth2/auth",
+        token_uri: "https://oauth2.googleapis.com/token",
+        auth_provider_x509_cert_url:
+          "https://www.googleapis.com/oauth2/v1/certs",
+        client_x509_cert_url:
+          "https://www.googleapis.com/robot/v1/metadata/x509/sn1tchapp%40sn1tchapp.iam.gserviceaccount.com",
+        universe_domain: "googleapis.com",
+      },
       scopes: ["https://www.googleapis.com/auth/drive.activity.readonly"],
     });
 
+    
     // Configurar autenticação para Directory API
     const directoryAuth = new google.auth.GoogleAuth({
-      keyFile: path.join(process.cwd(), process.env.SERVICE_ACCOUNT_KEY),
+      credentials: {
+        type: "service_account",
+        project_id: "sn1tchapp",
+        private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+        private_key: process.env.GOOGLE_PRIVATE_KEY,
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        auth_uri: "https://accounts.google.com/o/oauth2/auth",
+        token_uri: "https://oauth2.googleapis.com/token",
+        auth_provider_x509_cert_url:
+          "https://www.googleapis.com/oauth2/v1/certs",
+        client_x509_cert_url:
+          "https://www.googleapis.com/robot/v1/metadata/x509/sn1tchapp%40sn1tchapp.iam.gserviceaccount.com",
+        universe_domain: "googleapis.com",
+      },
       scopes: ["https://www.googleapis.com/auth/admin.directory.user.readonly"],
     });
 
